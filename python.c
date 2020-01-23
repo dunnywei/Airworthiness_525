13519cf596a88707554e864bdddcbf048ad1be06
dunnywei <wei_don@yahoo.com>
2020-01-23 09:49:54 -0500

    Create python.c
    update

---
+++ python.c
@@ -0,0 +1,7 @@
+->python run shell command and get output to variable
+https://stackoverflow.com/questions/20004307/run-a-complex-grep-command-with-python
+https://cmdlinetips.com/2014/03/how-to-run-a-shell-command-from-python-and-get-the-output/
+https://cmdlinetips.com/2014/03/how-to-run-a-shell-command-from-python-and-get-the-output/
+
+->read/write access with pthread in C
+https://stackoverflow.com/questions/55730428/reader-writer-problem-in-c-using-pthreads
