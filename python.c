7121df47add30a2eca9030f878262598ad6f6351
dunnywei <wei_don@yahoo.com>
2020-01-24 16:59:26 -0500

    update in python.c for C test

--- python.c
+++ python.c
@@ -5,3 +5,21 @@ https://cmdlinetips.com/2014/03/how-to-run-a-shell-command-from-python-and-get-t
 
 ->read/write access with pthread in C
 https://stackoverflow.com/questions/55730428/reader-writer-problem-in-c-using-pthreads
+
+->for C test
+/*couting number of 1's in 0000 0001b
+0000 000d &0000 0001==1
+0000 00d0 &0000 0010==2
+*/
+
+void counting(unsigned char input)
+{
+	int counter=0;
+	for(int a=0;i<8;i++)
+	{
+		if(input&(1<<i)==i)
+		{
+			counter++;
+		}
+	}
+}
